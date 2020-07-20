13d610a477372f76b0ad9c3e2bb8b1b38aaa169b
Patrick Brown <pjbrown05@gmail.com>
2020-07-20 18:11:55 -0500

    fixed delete, needed response. also, fixed for loop so the note ids were being updated when a note was deleted

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.post("/api/notes", (req, res) => {
         let noteArray = JSON.parse(data);
         // assigns the the value of the request object key 'id' as the index position of the new note in the noteArray array
         req.body.id = noteArray.length;
-        // 
+        // use the push method to add the newNote object to the noteArray
         noteArray.push(newNote);
         console.log(noteArray);
         if (err) throw err;
@@ -54,27 +54,31 @@ app.post("/api/notes", (req, res) => {
             if (err) throw err;
         });
     })
-    return res.json(null);
+    res.end();
 });
 
 app.delete("/api/notes/:id", (req, res) => {
     // req.params.id will be equal to the note delete button clicked on, stored in a variable selectedNote
     let selectedNote = req.params.id;
     fs.readFile(path.join(__dirname + "/db/db.json"), "utf8", (err, data) => {
-        console.log(selectedNote);
+        // console.log(selectedNote);
         let noteArray = JSON.parse(data);
+        // use the splice method to remove the object at the array index position (selecteNote) from the noteArray, deleting 1 object
         noteArray.splice(selectedNote, 1);
+        for (let i = 0; i < noteArray.length; i++) {
+            // noteArray.id = noteArray.length <- doesn't work because noteArray.id is the id of the noteArray and not the id of the note object
+            // need to used noteArray[i] to get the id of the object within the array and then .id for that object's id, then assign that to i for each iteration.
+            noteArray[i].id = i;
+        }
 
         if (err) throw err;
         fs.writeFile(path.join(__dirname + "/db/db.json"), JSON.stringify(noteArray), (err) => {
             if (err) throw err;
             console.table(noteArray)
+            res.end();
         });
     })
 });
-
-
-
 
 
 app.listen(PORT, () => {
