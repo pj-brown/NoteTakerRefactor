d411e38cf75e7079da162868c180b2e92f2c7914
Patrick Brown <pjbrown05@gmail.com>
2020-07-21 20:55:29 -0500

    added jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -3,19 +3,18 @@ const mysql = require("mysql");
 const util = require('util');
 require('dotenv').config();
 
-// const db = require('db')
-// db.connect({
-//   host: process.env.DB_HOST,
-//   username: process.env.DB_USER,
-//   password: process.env.DB_PASS
-// })
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_DATABASE
-});
+let connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
